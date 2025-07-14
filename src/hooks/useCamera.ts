f47b0f9a2cf2c6629f14fbe8d375001e1c345bee
behzad 1k <bhzd1k@gmail.com>
2025-07-14 20:27:12 +0330

    rear camera

--- src/hooks/useCamera.ts
+++ src/hooks/useCamera.ts
@@ -24,7 +24,7 @@ export function useCamera(selectedCamera: Camera) {
       setIsStreaming(false);
       return null;
     }
-  }, []);
+  }, [selectedCamera]);
 
   const stopCamera = useCallback(() => {
     if (stream) {
